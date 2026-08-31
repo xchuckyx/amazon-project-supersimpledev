972a5b431751a4f8bcf1702b0839835acdc6373c
xchuckyx <cdraefus@gmail.com>
2026-08-31 20:26:02 +0800

    JS Automated Test Cases for formatCurrency

--- tests/moneytest.js
+++ tests/moneytest.js
@@ -1,7 +1,37 @@
 import { formatCurrency } from '../script/utility/money.js'
 
+console.log('Test Suite: Format Currency');
+console.log('Convert cents into dollars');
 if (formatCurrency(2095) === '20.95') {
     console.log('Passed');
 } else {
     console.log('Failed');
 }
+
+console.log('Works with 0');
+if (formatCurrency(0) === '0.00') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
+
+console.log('Rounds up to the nearest cent - 5');
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
+
+console.log('Rounds up to the nearest cent - 4');
+if (formatCurrency(2000.4) === '20.00') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
+
+
+// TYPES OF TEST CASES
+// 1. Basic test cases = test if the code is working
+// 2. Edge cases = test with values that are tricky
+
+// Test Suite = Group of related tests
